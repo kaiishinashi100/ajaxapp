02db313d1d2dfc14b707481807f879195a8da30c
kaiishinashi100 <70202941+kaiishinashi100@users.noreply.github.com>
2021-01-13 12:48:46 +0900

    既読機能のサーバー側の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,18 @@ class PostsController < ApplicationController
     Post.create(content:  params[:content])
     redirect_to action: :index
   end
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked
+      #activerecordで更新する
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+    #更新したレコードを取得し直す
+    item = Post.find(params[:id])
+    #renderメソッドでchecked.jsへ返却している
+    render json: { post: item }
+  end
 end
